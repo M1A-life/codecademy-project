ae80000ff602f704e49b19702fc2473559f5efe1
M1A-life <kasich.alex@gmail.com>
2021-01-07 20:14:15 +0200

    added random generator of motivational quotes

---
+++ random-messages.js
@@ -0,0 +1,2 @@
+const list_of_messages = ['The Best Way To Get Started Is To Quit Talking And Begin Doing.', 'The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.', 'Don’t Let Yesterday Take Up Too Much Of Today.'];
+console.log(list_of_messages[Math.floor(Math.random()*3)]);
